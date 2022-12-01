c21799c06c5fa0ec7999b1a08895f7bcd269dab6
Yaremchuk <a.yaremchuk911@gmail.com>
2022-12-01 20:25:09 +0200

    01.12.2022 with Ant Design

--- src/utils/Validators/validate.js
+++ src/utils/Validators/validate.js
@@ -5,8 +5,6 @@ export const requered = value => {
     if(value) return undefined
     return 'Field is required'
 }
-
-
 //валидатор на максимальное значение длины ввода 
 export const maxLengthCreator = maxLength => (value) => (value && value.length > maxLength)
         ? `Max length is ${maxLength} symbols`
